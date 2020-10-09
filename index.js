eb5226cfdf3ff50709f50f08f40f2848f2e16574
Daniel Weibel <danielmweibel@gmail.com>
2020-10-09 17:04:39 +0200

    Improve log output

--- index.js
+++ index.js
@@ -18,24 +18,45 @@ async function main() {
     version = core.getInput('version');
     if (!version) {
       version = execSync('npm view ajv-cli version').toString().trim();
+      core.info(`Latest version of ajv-cli is ${version}`);
     } else if (!execSync('npm view ajv-cli versions').includes(version)) {
       fail(`${version} is not a valid version of ajv-cli`);
     }
 
     key = `ajv-cli-${version}-${process.env.RUNNER_OS}`;
     installDir = `${process.env.RUNNER_TEMP}/${key}`;
-    if (typeof await cache.restoreCache([installDir], key) === 'undefined') {
-      console.log("Cache miss");
-      console.log(execSync(`npm install --prefix ${installDir} ajv-cli@${version}`).toString());
+
+    // Restore installation directory from cache, if available
+    core.startGroup(`Checking cache for ajv-cli ${version}`);
+    _ = await cache.restoreCache([installDir], key)
+
+    // If cache has not been found
+    if (typeof _ === 'undefined') {
+      core.info("Not found");
+      core.endGroup();
+      // Create fresh installation of ajv-cli
+      core.startGroup(`Installing ajv-cli ${version}`)
+      cmd = `npm install -s --prefix ${installDir} ajv-cli@${version}`
+      core.info(`${cmd}:`);
+      core.info(execSync(cmd).toString().trim());
+      core.endGroup();
+      // Save installation directory to cache
+      core.startGroup(`Caching installation directory with key ${key}`);
       await cache.saveCache([installDir], key);
+      core.endGroup();
+    // If cache has been found and restored
     } else {
-      console.log("Cache hit");
+      core.endGroup();
+      core.info(`Found and restored ajv-cli ${version} from cache (${key})`);
     }
-    console.log("Verifying installation");
+    // Add ajv binary to PATH
     binDir = execSync(`npm bin --prefix ${installDir}`).toString().trim();
     core.addPath(binDir);
-    execSync(`PATH=${binDir}:$PATH ajv help`);
-    console.log("Installation successful");
+
+    // Verify installation
+    core.info("Verifying installation");
+    execSync(`PATH=${binDir}:$PATH ajv help`)
+    core.info(`Installation of ajv-cli ${version} successful`);
   } catch (err) {
     fail(err);
   }
