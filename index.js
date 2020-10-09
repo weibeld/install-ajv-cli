f0c9ede636423a50e68593715a8c2a4c9ee7454c
Daniel Weibel <danielmweibel@gmail.com>
2020-10-09 12:40:49 +0200

    Add optional 'version' input
    The 'version' input allows to specify a specific version of the ajv-cli
    package to install.
    
    If this input is set, the action verifies whether the supplied version is
    a valid version of ajv-cli and then installs this version.
    
    If this input is not set, the action determines and installs the latest
    version of the ajv-cli package.

--- index.js
+++ index.js
@@ -2,25 +2,37 @@ const core = require('@actions/core');
 const cache = require('@actions/cache');
 const { exec, execSync } = require("child_process");
 
+function fail(err) {
+  core.setFailed(err);
+}
+
 async function main() {
   try {
-    key = 'ajv-cli'
-    installDir = `${process.env.RUNNER_TEMP}/${key}}`
+
+    version = core.getInput('version');
+    if (!version) {
+      version = execSync('npm view ajv-cli version').toString().trim();
+    } else if (!execSync('npm view ajv-cli versions').includes(version)) {
+      fail(`${version} is not a valid version of ajv-cli`);
+    }
+
+    key = `ajv-cli-${version}`;
+    installDir = `${process.env.RUNNER_TEMP}/${key}`;
     if (typeof await cache.restoreCache([installDir], key) === 'undefined') {
       console.log("Cache miss");
-      console.log(execSync(`npm install --prefix ${installDir} ajv-cli`).toString());
+      console.log(execSync(`npm install --prefix ${installDir} ajv-cli@${version}`).toString());
       await cache.saveCache([installDir], key);
     } else {
       console.log("Cache hit");
     }
+    console.log("Verifying installation");
     binDir = execSync(`npm bin --prefix ${installDir}`).toString().trim();
     core.addPath(binDir);
     execSync(`PATH=${binDir}:$PATH ajv help`);
     console.log("Installation successful");
   } catch (err) {
-    core.setFailed(err);
+    fail(err);
   }
 }
 
 main();
-
